d23885d52b0165b95139d95e6db29e94092a9c7e
Pin3appl3ishan <sthaishan2004@gmail.com>
2024-09-06 23:52:18 +0545

    add state

--- steps/src/App.jsx
+++ steps/src/App.jsx
@@ -5,7 +5,7 @@ const messages = [
 ];
 
 export default function App() {
-  const step = 3;
+  const step = 2;
   return (
     <div className="steps">
       <div className="numbers">
@@ -17,10 +17,10 @@ export default function App() {
       <p className="message">Step {step}: {messages[step-1]}</p>
 
       <div className="buttons">
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }} onClick={() => alert("Previous")}>
           Previous
         </button>
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }} onClick={() => alert("Next")}>
           Next
         </button>
       </div>
