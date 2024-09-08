bda6f5fd363ed0112242e550bc51195e70cff4c2
Pin3appl3ishan <sthaishan2004@gmail.com>
2024-09-08 14:55:43 +0545

    add items form

--- travel-list/src/App.jsx
+++ travel-list/src/App.jsx
@@ -1,7 +1,7 @@
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 2, description: "Mobile", quantity: 12, packed: false },
+  { id: 3, description: "Mobile", quantity: 12, packed: false },
 ];
 const App = () => {
   return (
@@ -19,10 +19,24 @@ function Logo() {
 }
 
 function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(e);
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip?</h3>
-    </div>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>Add</button>
+    </form>
   );
 }
 
@@ -31,7 +45,7 @@ function PackagingList() {
     <div className="list">
       <ul>
         {initialItems.map((item) => (
-          <Item item={item} />
+          <Item item={item} key={item.id} />
         ))}
       </ul>
     </div>
@@ -41,7 +55,7 @@ function PackagingList() {
 function Item({ item }) {
   return (
     <li>
-      <span style={item.packed ? {textDecoration: "line-through"} : {}}>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity}
         {item.description}
       </span>
