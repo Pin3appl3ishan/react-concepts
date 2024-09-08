00959fa0f9161757e73d87aa1dac55e0b505cbb0
Pin3appl3ishan <sthaishan2004@gmail.com>
2024-09-08 13:24:21 +0545

    add components

--- travel-list/src/App.jsx
+++ travel-list/src/App.jsx
@@ -1,10 +1,15 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 2, description: "Mobile", quantity: 12, packed: false },
+];
 const App = () => {
   return (
     <div className="app">
       <Logo />
       <Form />
       <PackagingList />
-      <Stats/>
+      <Stats />
     </div>
   );
 };
@@ -22,7 +27,27 @@ function Form() {
 }
 
 function PackagingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? {textDecoration: "line-through"} : {}}>
+        {item.quantity}
+        {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 
 function Stats() {
