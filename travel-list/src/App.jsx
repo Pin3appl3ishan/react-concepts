50bd520b5238ab8960e8b1456f5dacaab88279d8
Pin3appl3ishan <sthaishan2004@gmail.com>
2024-09-10 12:45:03 +0545

    stats section

--- travel-list/src/App.jsx
+++ travel-list/src/App.jsx
@@ -28,7 +28,7 @@ const App = () => {
         onToggleItem={handleToggleItem}
         onDeleteItem={handleDeleteItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 };
@@ -113,11 +113,25 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length) {
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list✈️</em>
+      </p>
+    );
+  }
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
   return (
-    <footer>
+    <footer className="stats">
       <em>
-        You have {} items on your list, and you already packed {}
+        {percentage === 100
+          ? "You got everyting ! Ready to go"
+          : `🛒You have ${numItems} items on your list, and you already packed ${numPacked} (${percentage}%)`}
       </em>
     </footer>
   );
