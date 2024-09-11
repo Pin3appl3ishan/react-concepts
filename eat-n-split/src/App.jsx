e5c0bf1782845bbf17632db019f2c7a19d411554
Pin3appl3ishan <sthaishan2004@gmail.com>
2024-09-11 21:14:52 +0545

    cretae button component

--- eat-n-split/src/App.jsx
+++ eat-n-split/src/App.jsx
@@ -61,7 +61,23 @@ function Friend({ friend }) {
       )}
       {friend.balance === 0 && <p>You and {friend.name} are even</p>}
 
-      <button className="button">Select</button>
+      <Button>Select</Button>
     </li>
+  );
+}
+
+function Button({ children }) {
+  return <button className="button">{children}</button>;
+}
+
+function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>Friend name</label>
+      <input type="text" />
+
+      <label> Image URL</label>
+      <input type="text" />
+    </form>
   );
 }
